ce7d27e46b9ab5537cbf37c012c0571fd27985b1
moshi <moshiur@cefalo>
2020-12-05 15:59:53 +0600

    Last stone weight

---
+++ Solutions/_1046LastStoneWeight.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Algorithms;
+
+namespace LeetCode.Solutions
+{
+    public class _1046LastStoneWeight
+    {
+        public int LastStoneWeight(int[] stones)
+        {
+            var priorityQueue = new PriorityQueue(1000, stones);
+            var count = stones.Length;
+            while (count > 1)
+            {
+                var item1 = priorityQueue.Pop();
+                var item2 = priorityQueue.Pop();
+                var toInsert = item1 - item2;
+                priorityQueue.Insert(toInsert);
+                count--;
+            }
+            return priorityQueue.Pop();
+        }
+    }
+}
---
+++ Tests/_1046LastStoneWeightTests.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Solutions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+
+    public class _1046LastStoneWeightTests
+    {
+        _1046LastStoneWeight _1046LastStoneWeight;
+
+        public _1046LastStoneWeightTests()
+        {
+            _1046LastStoneWeight = new _1046LastStoneWeight();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var items = new int[] { 2, 7, 4, 1, 8, 1 };
+            var lastStoneStands = _1046LastStoneWeight.LastStoneWeight(items);
+            Assert.Equal(1, lastStoneStands);
+
+            items = new int[] {3, 7, 2};
+            lastStoneStands = _1046LastStoneWeight.LastStoneWeight(items);
+            Assert.Equal(2, lastStoneStands);
+
+            items = new int[] {9, 3, 2, 10 };
+            lastStoneStands = _1046LastStoneWeight.LastStoneWeight(items);
+            Assert.Equal(0, lastStoneStands);
+        }
+    }
+}
